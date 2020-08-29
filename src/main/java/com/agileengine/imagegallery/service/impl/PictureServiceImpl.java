696621c4af24fdeac097dcf8a7f2b2bb1bc26d49
Dmitry Pratsun <pratsundv@gmail.com>
2020-08-29 20:37:06 +0300

    PictureServiceImpl have been implemented

--- src/main/java/com/agileengine/imagegallery/service/impl/PictureServiceImpl.java
+++ src/main/java/com/agileengine/imagegallery/service/impl/PictureServiceImpl.java
@@ -1,21 +1,26 @@
 package com.agileengine.imagegallery.service.impl;
 
+import com.agileengine.imagegallery.cache.PictureCache;
 import com.agileengine.imagegallery.domain.Picture;
 import com.agileengine.imagegallery.service.PictureService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@AllArgsConstructor
 public class PictureServiceImpl implements PictureService {
+    private final PictureCache cache;
+
     @Override
     public List<Picture> getPicturesByPage(int page) {
-        return null;
+        return cache.getByPage(page);
     }
 
     @Override
     public Picture getPictureById(String id) {
-        return null;
+        return cache.getById(id);
     }
 
     @Override
